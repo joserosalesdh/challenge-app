b57d074e9107293e05b143b4cfcba9c9f04b0b95
joserosalesdh <jm.rosales92@gmail.com>
2020-10-07 15:01:47 -0300

    no soluciono como hacer el find aca

--- src/components/CarScreen.js
+++ src/components/CarScreen.js
@@ -1,18 +1,49 @@
 import React from 'react';
-import { Redirect, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useRequest } from '../hooks/useRequest';
 
 const CarScreen = () => {
 
     const { carId } = useParams();
 
-    const { data } = useRequest(`https://challenge.agenciaego.tech/models${carId}`);
+    const idCorrecto = carId - 1;
+
     console.log(carId)
+    console.log(idCorrecto)
+
+    const { data, isLoaded, error } = useRequest(`https://challenge.agenciaego.tech/models`);
+
+    // console.log(data[carId - 1])
+
+    const autoEncontrado = data.find(item => item.id === carId);
+
+    console.log(autoEncontrado)
 
     return (
         <div>
 
-            <h1>Car Screen {carId} </h1>
+            {error && <div>Something went wrong ...</div>}
+
+            {isLoaded ? (
+                <div>Loading...</div>
+            ) : (
+
+                    <>
+                        <h1>Car Screen {carId} </h1>
+
+                        {
+                            data.find(item => (
+                                item.id === carId
+                            ))
+
+                        }
+
+                    </>
+
+                )}
+
+
+
 
         </div>
     )
